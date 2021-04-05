aaf7bdb26ea7191f60e55894f939dc2f39ff6ab4
jungjoongi <z2046818>
2021-04-06 01:30:32 +0900

    dev setting

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ APP_NAME=debugApp
 BUILD_PATH=/home/pi/server/build/debugApp
 JAR_NAME=$BUILD_PATH$debugApp.jar
 
+cd /var/lib/jenkins/workspace/debugApp/debugApp
 
 echo "chmod +x gradlew"
 chmod +x gradlew
