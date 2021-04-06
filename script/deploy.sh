8eb932142706470499b5052d20a359110ce98ac9
jungjoongi <z2046818>
2021-04-06 23:43:08 +0900

    dev setting

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,12 +1,6 @@
 echo "debugApp deploy"
 echo "=============="
 
-If [ ! -d /home/pi/server/application/debugapp ] ; then
-  echo '/home/pi/server/application/debugapp 디렉토리가 없습니다. 디렉토리를 생성합니다.'
-  echo 'mkdir /home/pi/server/application/debugapp'
-  mkdir /home/pi/server/application/debugapp
-fI
-
 echo 'cp /var/lib/jenkins/workspace/debugApp/debugApp/build/libs/debugapp.jar /home/pi/server/application/debugapp'
 cp /var/lib/jenkins/workspace/debugApp/debugApp/build/libs/debugapp.jar /home/pi/server/application/debugapp
 cp /var/lib/jenkins/workspace/debugApp/debugApp/src/main/resources/env/prd01/application-prd01.yml /home/pi/server/application/debugapp
