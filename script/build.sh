d5513b866185fc02a29e55e9be11f84417d3e4aa
jungjoongi <zx3620@gmail.com>
2021-04-06 23:31:15 +0900

    dev setting

--- script/build.sh
+++ script/build.sh
@@ -1,9 +1,5 @@
 echo "debugApp build"
 echo "=============="
 
-cd /var/lib/jenkins/workspace/debugApp/debugApp
-
-echo "./gradlew bootjar"
-./gradlew bootjar
-
-
+echo '/var/lib/jenkins/workspace/debugApp/debugApp/gradlew bootjar'
+/var/lib/jenkins/workspace/debugApp/debugApp/gradlew bootjar
--- script/test.sh
+++ script/test.sh
@@ -1,14 +1,11 @@
 echo "debugApp test"
-
-cd /var/lib/jenkins/workspace/debugApp/debugApp
-
 echo "chmod +x gradlew"
 chmod +x gradlew
 
 echo "dos2unix gradlew"
 dos2unix gradlew
 
-echo "./gradlew test"
-./gradlew test
+echo "/var/lib/jenkins/workspace/debugApp/debugApp/gradlew test"
+/var/lib/jenkins/workspace/debugApp/debugApp/gradlew test
 
 
