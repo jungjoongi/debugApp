732164fdc904f7dea0b51230bb75f70c944a5e9e
jungjoongi <z2046818>
2021-04-07 00:04:05 +0900

    dev setting

--- script/build.sh
+++ script/build.sh
@@ -2,6 +2,8 @@ echo "debugApp build"
 echo "=============="
 
 cd /var/lib/jenkins/workspace/debugApp/debugApp
+cp /var/lib/jenkins/workspace/debugApp/debugApp/src/main/resources/env/prd01/application-prd01.yml /var/lib/jenkins/workspace/debugApp/debugApp/src/main/resources
+cp /var/lib/jenkins/workspace/debugApp/debugApp/src/main/resources/env/prd02/application-prd02.yml /var/lib/jenkins/workspace/debugApp/debugApp/src/main/resources
 
 echo './gradlew bootjar'
 ./gradlew bootjar
