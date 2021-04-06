78c84c74b16dc9e8597dde71959eec56f0be4381
jungjoongi <zx3620@gmail.com>
2021-04-06 23:31:15 +0900

    dev setting

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,16 @@
-echo "debugApp build"
+echo "debugApp deploy"
 echo "=============="
 
-cd /var/lib/jenkins/workspace/debugApp/debugApp
+If [ ! -d /home/pi/server/application/debugapp ] ; then
+  echo '/home/pi/server/application/debugapp 디렉토리가 없습니다. 디렉토리를 생성합니다.'
+  echo 'mkdir /home/pi/server/application/debugapp'
+  mkdir /home/pi/server/application/debugapp
+fI
 
-echo "./gradlew bootjar"
-./gradlew bootjar
+echo 'cp /var/lib/jenkins/workspace/debugApp/debugApp/build/libs/debugapp.jar /home/pi/server/application/debugapp'
+cp /var/lib/jenkins/workspace/debugApp/debugApp/build/libs/debugapp.jar /home/pi/server/application/debugapp
+cp /var/lib/jenkins/workspace/debugApp/debugApp/src/main/resources/env/prd01/application-prd01.yml /home/pi/server/application/debugapp
+cp /var/lib/jenkins/workspace/debugApp/debugApp/src/main/resources/env/prd02/application-prd02.yml /home/pi/server/application/debugapp
 
-
+echo 'sh /home/pi/server/application/debugapp/start.sh'
+sh /home/pi/server/application/debugapp/start.sh
