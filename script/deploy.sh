01634c2bf1c6053164d9428382e8813ea27c9be0
jungjoongi <z2046818>
2021-04-07 20:32:01 +0900

    dev setting

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,8 +3,6 @@ echo "=============="
 
 echo 'cp /var/lib/jenkins/workspace/debugApp/debugApp/build/libs/debugapp.jar /home/pi/server/application/debugapp'
 cp /var/lib/jenkins/workspace/debugApp/debugApp/build/libs/debugapp.jar /home/pi/server/application/debugapp
-cp /var/lib/jenkins/workspace/debugApp/debugApp/src/main/resources/env/prd01/application-prd01.yml /home/pi/server/application/debugapp
-cp /var/lib/jenkins/workspace/debugApp/debugApp/src/main/resources/env/prd02/application-prd02.yml /home/pi/server/application/debugapp
 
 echo 'sh /home/pi/server/application/debugapp/start.sh'
 sh /home/pi/server/application/debugapp/start.sh
