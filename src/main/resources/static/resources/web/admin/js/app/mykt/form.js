fc360b90b74822cb896dd73c2b2d931362913035
jungjoongi <zx3620@gmail.com>
2021-04-12 23:20:30 +0900

    dev setting

--- src/main/resources/static/resources/web/admin/js/app/mykt/form.js
+++ src/main/resources/static/resources/web/admin/js/app/mykt/form.js
@@ -4,6 +4,7 @@ let form = {
     },
     create : function () {
         this.submit();
+        this.getFileName();
     },
     isProgress : false,
     submit : function() {
@@ -11,20 +12,47 @@ let form = {
             if(!isProgress) {
                 form.isProgress = true;
                 if(this.validation()) {
-                    $("#app-from").submit();
-                    form.isProgress = false;
+                        let formData = $('#app-from').serialize() // serialize 사용
+                        $.ajax({
+                            url: "/admin/app/mykt/insert",
+                            enctype: 'multipart/form-data',
+                            type: "POST",
+                            cache: false,
+                            data: formData, // data에 바로 serialze한 데이터를 넣는다.
+                            success: function(data){
+                                form.isProgress = false;
+                                console.log(data)
+                            },
+                            error: function (request, status, error){
+                                form.isProgress = false;
+                                console.log(error)
+                            }
+                        })
+
                 }
             }
         });
     },
     validation : function() {
+        let fileInput = $("#exampleInputFile").val();
+        if("" == fileInput) {
+            alert("파일을 업로드 해주세요");
+            $("#exampleInputFile").focus();
+            return false;
+        }
 
-
+    }
+    ,
+    getFileName : function() {
+        $("#exampleInputFile").change(function(e){
+            $("#exampleInputFile-label").html(e.target.files[0].name);
+        });
     }
 }
 $(document).ready(function() {
-    list.init();
-    list.create();
+    form.init();
+    form.create();
 });
+
 
 
