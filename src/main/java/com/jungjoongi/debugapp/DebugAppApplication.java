022d7bca8afb04ca1eaa3a4ae08634b1cab28690
jungjoongi <zx3620@gmail.com>
2021-04-12 19:51:39 +0900

    dev setting

--- src/main/java/com/jungjoongi/debugapp/DebugAppApplication.java
+++ src/main/java/com/jungjoongi/debugapp/DebugAppApplication.java
@@ -3,8 +3,10 @@ package com.jungjoongi.debugapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableJpaAuditing // JPA Auditing 활성화
+@EnableWebSecurity
 @SpringBootApplication
 public class DebugAppApplication {
 
